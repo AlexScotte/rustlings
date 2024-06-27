17263a245ae949c686994344d1aa0bf66c3e3eec
AlexScotte <scottealexandre@gmail.com>
2024-06-27 17:26:56 +0200

    vecs completed !

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -7,11 +7,26 @@
 //
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+    In Rust, there are two ways to define a Vector.
+    1. One way is to use the `Vec::new()` function to create a new vector
+    and fill it with the `push()` method.
+    2. The second way, which is simpler is to use the `vec![]` macro and
+    define your elements inside the square brackets.
+
+    Check this chapter: https://doc.rust-lang.org/stable/book/ch08-01-vectors.html
+    of the Rust book to learn more.
+*/
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v: Vec<i32> = vec![10, 20, 30, 40]; // TODO: declare your vector here with the macro for vectors
+    // OR
+    // let mut v = Vec::new();
+    // v.push(10); 
+    // v.push(20); 
+    // v.push(30); 
+    // v.push(40); 
 
     (a, v)
 }
--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -7,13 +7,30 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+    In the first function we are looping over the Vector and getting a reference to
+    one `element` at a time.
+
+    To modify the value of that `element` we need to use the `*` dereference
+    operator. You can learn more in this chapter of the Rust book:
+    https://doc.rust-lang.org/stable/book/ch08-01-vectors.html#iterating-over-the-values-in-a-vector
+
+    In the second function this dereferencing is not necessary, because the `map`
+    function expects the new value to be returned.
+
+    After you've completed both functions, decide for yourself which approach you
+    like better.
+
+    What do you think is the more commonly used pattern under Rust developers?
+*/
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element = *element * 2;
+        // OR
+        // *element *= 2
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -24,7 +41,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element * 2
     }).collect()
 }
 
